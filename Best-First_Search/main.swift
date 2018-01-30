ab2a6a510855370af15cd7caf4de7abc80f3828c
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-01-29 23:19:27 -0600

    Initial Commit

---
+++ Best-First_Search/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Best-First_Search
+//
+//  Created by Jonathan Daniel on 1/29/18.
+//  Copyright © 2018 Jonathan Daniel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
