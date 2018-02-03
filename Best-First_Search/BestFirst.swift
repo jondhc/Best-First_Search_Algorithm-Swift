26e80b666199b386bc26966dda4643fcf2347a77
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-02-03 13:53:53 -0600

    Initial Commit

---
+++ Best-First_Search/BestFirst.swift
@@ -0,0 +1,11 @@
+//
+//  BestFirst.swift
+//  Best-First_Search
+//
+//  Created by Jonathan Daniel on 1/30/18.
+//  Copyright © 2018 Jonathan Daniel. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ Best-First_Search/Situation.swift
@@ -0,0 +1,14 @@
+//
+//  Situation.swift
+//  Best-First_Search
+//
+//  Created by Jonathan Daniel on 1/30/18.
+//  Copyright © 2018 Jonathan Daniel. All rights reserved.
+//
+
+import Foundation
+
+public class Situation{
+//    let matrix: Matrix = [[3],[3]]
+    
+} //end situation
